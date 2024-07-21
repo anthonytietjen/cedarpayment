5d13656adc755a04b6ce33703ebd69242620f3c7
Anthony Tietjen <tietjen.anthony@gmail.com>
2024-07-21 01:58:17 -0600

    Created Thank You page

---
+++ src/app/thankyou/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center pt-[86px] pb-[40px]">
+        <div className="w-full max-w-[376px] flex-col">
+          <h1
+            className="text-[28px] mb-[16px] text-center text-[#13126C] font-bold"
+            style={{ fontFamily: "georgia" }}
+          >
+            Thank you for your payment!
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
