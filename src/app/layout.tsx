d622baf31335b89a339aaebe3010402a01f1df01
Anthony Tietjen <tietjen.anthony@gmail.com>
2024-07-25 13:59:10 -0600

    Remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import PageLayout from "./components/PageLayout";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ABC Health System",
